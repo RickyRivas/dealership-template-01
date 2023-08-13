8b64212dc04f080698df3ff93683be07ba6addcf
Ricky Rivas <rickyrivas918@gmail.com>
2023-08-13 00:56:38 -0500

    hero & nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,14 +24,18 @@
 </svelte:head>
 
 <section id="landing">
+  <picture>
+    <source media="(max-width: 767px)" srcset="/hero-mobile.jpg" />
+    <source media="(min-width: 768px)" srcset="/hero.jpg" />
+    <img aria-hidden="true" decoding="async" src="/hero.jpg" alt="hero" width="800" height="450" />
+  </picture>
   <div class="content">
-    <span>Made with Sveltekit</span>
-    <h1>Web development,<br /> streamlined</h1>
+    <span>Highest Rated Dealer</span>
+    <h1>Car Dealership in Tulsa, Ok</h1>
     <p>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates adipisci et magni
       laborum, voluptatibus omnis soluta numquam neque eaque inventore.
     </p>
-    <a href="/" class="cta">Read the docs</a>
+    <a href="/" class="cta">Explore Vehicles</a>
   </div>
-  <img class="hero" src="/img01.jpg" alt="laptop" width="500" height="610" decoding="async" />
 </section>
